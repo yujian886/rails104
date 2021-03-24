3faef7eadee382a0b218035f18f2f772968454bc
yujian886 <yjxuexi@gmail.com>
2021-03-24 21:42:20 +0800

    add user_id_to_group

---
+++ db/migrate/20210324132514_add_user_id_group.rb
@@ -0,0 +1,5 @@
+class AddUserIdGroup < ActiveRecord::Migration[6.1]
+  def change
+    add_column :groups, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_145023) do
+ActiveRecord::Schema.define(version: 2021_03_24_132514) do
 
   create_table "groups", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
